32a4fd15e25e97466291fee68f37116535314ff5
CharlieDigital <chen.charles.c@gmail.com>
2022-01-15 12:00:02 -0500

    Reformatted

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,14 +6,14 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
+    plugins: [
+        vue({
+            template: { transformAssetUrls }
+        }),
 
-    quasar({
-      autoImportComponentCase: 'pascal',
-      sassVariables: 'src/quasar-variables.sass'
-    })
-  ]
+        quasar({
+            autoImportComponentCase: 'pascal',
+            sassVariables: 'src/quasar-variables.sass'
+        })
+    ]
 })
